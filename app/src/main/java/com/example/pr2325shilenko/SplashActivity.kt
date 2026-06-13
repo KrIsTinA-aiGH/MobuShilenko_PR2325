4a186839ef8ea9bc9b13290d8809b29a24533e3d
Kristina <shilenko.c@mail.ru>
2026-06-13 14:24:39 +0700

    запуск загрузочного экрана

--- app/src/main/java/com/example/pr2325shilenko/SplashActivity.kt
+++ app/src/main/java/com/example/pr2325shilenko/SplashActivity.kt
@@ -3,14 +3,15 @@ package com.example.pr2325shilenko
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
-        // НЕ вызываем setContentView()! View берется из темы
+        Thread.sleep(800)
 
-        // Сразу переходим к MainActivity
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
